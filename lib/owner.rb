438745feb2082a1ceb5861ca104469ba54c2284d
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-17 04:49:01 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_accessor 
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    # raise NoMethodError if @name != name
+  end
+  
+  def say_species
+   return "I am a #{@species}." 
+  end
+  
+  def self.all 
+    # binding.pry
+   
+  end
+    
+  
 end
